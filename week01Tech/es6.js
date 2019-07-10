dd8ce5762bb376dd9fa42cde45a30fa8df7e9f4e
mohamed gassama <bouyagassama@gmail.com>
2019-07-09 20:37:35 -0400

    var and let example

--- week01Tech/es6.js
+++ week01Tech/es6.js
@@ -9,5 +9,36 @@ arr.forEach(function(el) {
 arr.map(el => el + 1);
 
 // Declaring variable in JS
+// var, let, const
 
-// var, let const
+// var variable are not generally block-scope
+
+// variable is define in the global scope
+var i = 0;
+console.log(i);
+
+// variable that have the same name in both global and local scope can be overwritten
+var i = 0;
+console.log(i);
+
+function example() {
+  var i = 1;
+  console.log(i);
+}
+example();
+
+// output ii 1
+
+// variable is define in the local scope
+function example1() {
+  var i = 0;
+  console.log(i);
+}
+example1();
+
+// let variable are block-scope
+function example2() {
+  let i = 0;
+  console.log(i);
+}
+example2();
