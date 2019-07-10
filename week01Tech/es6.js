bb5976b0dc078f9527b5096f592163ffebcd4c65
mohamed gassama <bouyagassama@gmail.com>
2019-07-10 19:54:19 -0400

    varletconst exercise

--- week01Tech/es6.js
+++ week01Tech/es6.js
@@ -52,3 +52,55 @@ function example3() {
   console.log(i);
 }
 example2();
+
+// varLetConst exercices
+
+const _replaceMe_ = null;
+
+// Remove _replaceMe_ and add your own code so that each variable is the type that its name indicates
+const numberVar = 1;
+const stringVar = 'Hello World';
+const booleanVar = true;
+const nullVar = null;
+const undefinedVar = undefined;
+
+// Remove _replaceMe_ and use math operators to assign the following variables. Use Math methods where ever possible.
+const pi = Math.PI;
+const piPlusOne = pi + 1;
+const piMinusOne = pi - 1;
+const piDividedByThree = pi / 1;
+const piSquared = Math.pow(pi, 2);
+const piRoundedDown = Math.floor(pi);
+const piRoundedUp = Math.ceil(pi);
+
+// Remove _replaceMe_ and assign the variables below using x and y.
+const x = 5;
+const y = 4;
+const isXLessThanY = x < y;
+const xMinusY = x - y;
+const yDividedByX = y / x;
+const xAsAString = 'x';
+const YToThePowerOfX = Math.pow(y, x);
+const yRoundedUp = Math.ceil(y);
+
+/* Use `let` to keep x from 'leaking' out of our conditional block.
+ * */
+let k = 1;
+
+if (k === 1) {
+  let k = 2;
+}
+
+console.log(k); // should output 1
+
+/* Use `const` so `x` can't change
+ *
+ * Consider renaming to const convention
+ * */
+const j = 1;
+
+if (true) {
+  const j = 2; // should raise a TypeError
+}
+
+console.log(j); // 1
