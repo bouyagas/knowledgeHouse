c6a33ebd3955b1a7c5c986ef7b3fa8c585058878
mohamed gassama <bouyagassama@gmail.com>
2019-07-09 20:09:01 -0400

    initial commit

---
+++ week01Tech/es6.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4, 5];
+
+// Mutative iterator
+arr.forEach(function(el) {
+  console.log(el);
+});
+
+// Unmutative iterator
+arr.map(el => el + 1);
+
+// Declaring variable in JS
+
+// var, let const
