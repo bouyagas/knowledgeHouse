be4339c7850b377f814b7dce77213ede8bcd8dd1
mohamed gassama <bouyagassama@gmail.com>
2019-07-11 19:08:36 -0400

    commandLine exercises

---
+++ week01Tech/commandLine/commandLine.js
@@ -0,0 +1,5 @@
+// commannd "ls" list the current directory
+// command "cd" change the current directory
+// command "mkdir" creat a new directory
+// command "pwd" print the working directory
+// command 'touch  create a new file
