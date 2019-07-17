d5183c932fd1fb1345879964ae3c4aff483d9e5b
mohamed gassama <bouyagassama@gmail.com>
2019-07-17 01:18:31 -0400

    Js conditions

---
+++ week02Tech/jsConditon.js
@@ -0,0 +1,13 @@
+// condition
+if (condition) {
+  // code execute
+}
+
+if (condition === true) {
+  // the code in this block will execute
+} else if (condition === false) {
+  // the code in this block will execute
+} else {
+  // none of those condition is true
+  // the code in this block will execute
+}
---
+++ week02Tech/jsVariable.js
@@ -0,0 +1,52 @@
+/* ==== Challenge 1: ====
+  - Create a variable named `name`
+  - Assign it's value to your first and last name 
+  - Does this variable need to be a constant? It depends completely on you! It should be a const variable IF you intend to never change your name.
+*/
+
+// code for `name` goes here
+// const name = "Mohamed Gassama";
+// console.log(name)
+
+/* ==== Challenge 2: ====
+  - Create a variable called `age`
+  - UNLESS you've descovered a very important secret in life, you should  probably allow for `mutability` here.
+*/
+
+// code for `age` goes here
+
+// let age = 28;
+// console.log(age)
+
+/* ==== Challenge 3: ====
+  - Create a variable called `myLocation`
+  - Base the value on where you're currently sitting. Consider if this is something that could change eventually.
+*/
+
+// code for `myLocation` goes here
+
+let myLocation = 'Classroom';
+
+/* ==== Challenge 4: ====
+  - Create a variable called `faveBook` 
+  - Consider if this is some data about yourself that could change.
+*/
+
+// code for `faveBook` goes here
+let faveBook = 'Zero to one';
+
+/* ==== Challenge 5: ====
+  - Create a variable called `faveBand` 
+  - Consider if this is some data about yourself that could change.
+*/
+
+// code for `faveBand` goes here
+// let faveBand
+
+/* ==== Challenge 6: ====
+  - Problem create a variable called `faveFood` 
+  - Consider if this is some data about yourself that could change.
+*/
+
+// code for `faveFood` goes here
+// let faveFood;
